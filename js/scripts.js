dad6dc2dba0723ce0660add39b51288ef1401948
moraleservices <gwoflsoluciones@gmail.com>
2026-02-19 15:16:01 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,34 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+<script>
+const images = [
+    "imagenes/trabajo1.jpg",
+    "imagenes/trabajo2.jpg",
+    "imagenes/trabajo3.jpg",
+    "imagenes/trabajo4.jpg",
+    "imagenes/trabajo5.jpg",
+    "imagenes/trabajo6.jpg"
+];
+
+let currentIndex = 0;
+
+function openLightbox(index) {
+    currentIndex = index;
+    document.getElementById("lightbox").style.display = "flex";
+    document.getElementById("lightbox-img").src = images[currentIndex];
+}
+
+function closeLightbox() {
+    document.getElementById("lightbox").style.display = "none";
+}
+
+function changeImage(direction) {
+    currentIndex += direction;
+    if (currentIndex < 0) currentIndex = images.length - 1;
+    if (currentIndex >= images.length) currentIndex = 0;
+    document.getElementById("lightbox-img").src = images[currentIndex];
+}
+
+</script>
